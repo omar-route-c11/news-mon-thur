e3076197d44fc9ca752015c652cc89be905778f8
Omar Assem <omar@192.168.1.6>
2024-08-26 21:32:50 +0300

    Implement home screen switching views logic

--- lib/category/category_details.dart
+++ lib/category/category_details.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CategoryDetails extends StatelessWidget {
-  const CategoryDetails({super.key});
+  const CategoryDetails(
+    this.categoryId, {
+    super.key,
+  });
+
+  final String categoryId;
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      color: Colors.red,
+    );
   }
 }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:news/app_theme.dart';
 import 'package:news/category/categories_grid.dart';
+import 'package:news/category/category_details.dart';
+import 'package:news/category/category_model.dart';
 import 'package:news/drawer/home_drawer.dart';
+import 'package:news/settings/settings_tab.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   static const String routeName = '/';
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,9 +30,28 @@ class HomeScreen extends StatelessWidget {
         appBar: AppBar(
           title: const Text('News App'),
         ),
-        drawer: const HomeDrawer(),
-        body: const CategoriesGrid(),
+        drawer: HomeDrawer(onItemSelected: onDrawerItemSelected),
+        body: selectedCategory != null
+            ? CategoryDetails(selectedCategory!.id)
+            : selectedDrawerItem == DrawerItem.categories
+                ? CategoriesGrid(onCategorySelected: onCategorySelected)
+                : const SettingsTab(),
       ),
     );
+  }
+
+  DrawerItem selectedDrawerItem = DrawerItem.categories;
+  CategoryModel? selectedCategory;
+
+  void onDrawerItemSelected(DrawerItem drawerItem) {
+    selectedDrawerItem = drawerItem;
+    selectedCategory = null;
+    setState(() {});
+    Navigator.of(context).pop();
+  }
+
+  void onCategorySelected(CategoryModel category) {
+    selectedCategory = category;
+    setState(() {});
   }
 }
